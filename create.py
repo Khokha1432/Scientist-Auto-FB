acc6bbfbf6af1ca0761bf322b42c45e96a9a83b9
Khokha1432 <96960019+Khokha1432@users.noreply.github.com>
2023-08-26 11:43:53 -0700

    Update create.py

--- create.py
+++ create.py
@@ -215,7 +215,7 @@ def Create():
         headers1 = {
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-encoding': 'gzip, deflate, br'
-            'accept-language': 'en-US, en;q=0.9',
+            'accept-language': 'en-US', 'en;q=0.9',
             'cache-control': 'max-age=0',
             'sec-ch-prefers-color-scheme': 'light',
             'sec-ch-ua': '"Not: A-Brand"; v="24", "Chromium";V="116"',
