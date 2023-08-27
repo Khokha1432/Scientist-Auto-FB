9d28b78c1950b9ab5e9fd9f78aa7eb2154fa5aa1
Khokha1432 <96960019+Khokha1432@users.noreply.github.com>
2023-08-27 18:07:26 +0500

    Update create.py

--- create.py
+++ create.py
@@ -388,7 +388,7 @@ def Create():
            hd = {'authority': 'mbasic.facebook.com', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'accept-language': 'en-US,en;q=0.9', 'cache-control': 'max-age=0', 'origin': 'https://mbasic.facebook.com', 'referer': 'https://mbasic.facebook.com/reg', 'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"Android"', 'sec-fetch-dest': 'document', 'sec-fetch-mode': 'navigate', 'sec-fetch-site': 'same-origin', 'sec-fetch-user': '?1', 'upgrade-insecure-requests': '1', 'user-agent':random_ua()}
            if "9":
               pas=random.choice(m).replace('|','').lower()+rnd(1111,9999)
-              requests=process(pas,mmail)
+              requests=process(pas,mmail,r)
               if requests=="chk":
                 cp+=1
                 pass
